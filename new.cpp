4c188a66ef0fbe4d937cc63030fc4b54336ffa5f
Paarthbathla338 <Paarth.bathla@outlook.com>
2021-07-02 08:43:34 +0530

    Solved Hacckerank Problem on i/o

---
+++ new.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a,b,c;
+    cout<<"Enter Your Number for addition\n";
+    cin>>a;
+    cout<<"Enter Your 2nd Number for addition\n";
+    cin>>b;
+    cout<<"Enter Your 3rd Number for addition\n";
+    cin>>c;
+
+
+    cout<<"Your Answer is: "<<a+b+c;   
+    return 0;
+}
